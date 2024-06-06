ea8db1f6dec0674da839f409b573660e76247a4b
Ettotto94 <Ace09040805@gmail.com>
2024-06-06 22:00:19 +0900

    デプロイ

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
-  base: './', // 基本パスの設定
+  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true })
+  ],
   resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  }
 });
